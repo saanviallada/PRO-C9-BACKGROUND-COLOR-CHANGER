8079299f51cb255be77b3bb9d2b544fc8b60fd1f
saanviallada <101298991+saanviallada@users.noreply.github.com>
2022-03-14 20:16:05 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+   background("yellow");
+
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    background("red");
+  }
+  drawSprites();
+}
+
+
+
+
